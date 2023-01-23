9a5c2f01f0cfb49cad4b50c2fec18df5a74119ab
seonb2n <626ksb@gmail.com>
2023-01-23 21:20:11 +0900

    add frontend code

---
+++ src/test/java/com/example/springsns/SpringSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
