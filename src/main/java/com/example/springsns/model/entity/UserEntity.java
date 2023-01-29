d44ac93ad91fb3ec5ad12c54bb2e92c6b77f1af4
seonb2n <626ksb@gmail.com>
2023-01-29 12:32:23 +0900

    - UserEntity 업데이트

--- src/main/java/com/example/springsns/model/entity/UserEntity.java
+++ src/main/java/com/example/springsns/model/entity/UserEntity.java
@@ -2,23 +2,49 @@ package com.example.springsns.model.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Entity
-@Table
+@Table(name ="\"user\"")
 @Getter
 @Setter
+@SQLDelete(sql = "UPDATE \"user\" SET deleted_at = NOW() WHERE id =?")
+@Where(clause = "deleted_at is NULL")
 public class UserEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column()
+    @Column(name = "user_name")
     private String userName;
 
-    @Column()
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "registered_at")
+    private Timestamp registerAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void registeredAt() {
+        this.registerAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
 }
